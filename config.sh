8b71b187cf8e6e786c17a10fb810f9ad2ac7299f
Simeia Macedo <simeiamacedo@@windowslive.com>
2024-07-24 17:35:48 -0300

    Alteraçoes no codigo para rodar os containers

--- config.sh
+++ config.sh
@@ -12,16 +12,16 @@ systemctl start docker
 systemctl enable docker
 
 # Instalação do Curl
-sudo apt install -y curl
- 
-# Instalação do Docker Compose 
-sudo curl -L "https://github.com/docker/compose/releases/download/v2.29.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+apt install -y curl
 
-# Dando permisão de execução ao Docker-compose
+# Instalação do Docker Compose 
+curl -L "https://github.com/docker/compose/releases/download/v2.29.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+# Dando permissão de execução ao Docker Compose
 chmod +x /usr/local/bin/docker-compose
 
-# Criação da pasta da aplicações
-mkdir /opt/aplicacoes
+# Criação da pasta para as aplicações
+mkdir -p /opt/aplicacoes
 
 # Criar o arquivo docker-compose.yml
 cat <<EOF > /opt/aplicacoes/docker-compose.yml
@@ -32,15 +32,20 @@ services:
     image: mysql:5.7
     volumes:
       - db_data:/var/lib/mysql
+      - ./init.sql:/docker-entrypoint-initdb.d/init.sql
+    ports:
+      - "3306:3306"
     restart: always
     environment:
       MYSQL_ROOT_PASSWORD: example
       MYSQL_DATABASE: wordpress
-      MYSQL_USER: root
+      MYSQL_USER: simeia-macedo
       MYSQL_PASSWORD: GAud4mZby8F3SD6P
 
   wordpress:
     image: wordpress:latest
+    depends_on:
+      - db
     volumes:
       - wordpress_data:/var/www/html
     ports:
@@ -48,13 +53,20 @@ services:
     restart: always
     environment:
       WORDPRESS_DB_HOST: db:3306
-      WORDPRESS_DB_USER: wordpress
+      WORDPRESS_DB_USER: simeia-macedo
       WORDPRESS_DB_PASSWORD: GAud4mZby8F3SD6P
-      WORDPRESS_DB_NAME: root
+      WORDPRESS_DB_NAME: wordpress
 
 volumes:
   db_data:
   wordpress_data:
+EOF
+
+# Criando arquivo init.sql
+cat <<EOF > /opt/aplicacoes/init.sql
+
+GRANT ALL PRIVILEGES ON WORDPRESS TO 'simeia'@'%';
+FLUSH PRIVILEGES;
 EOF
 
 # Iniciar os containers com o Docker Compose
